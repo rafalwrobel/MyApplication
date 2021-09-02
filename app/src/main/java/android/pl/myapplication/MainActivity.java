7dfd846e0132ff956a81ff2a22999fa27a6363d7
Rafał Wróbel <35737823+rafalwrobel@users.noreply.github.com>
2021-09-02 18:46:54 +0200

    Remote Controller - Client

--- app/src/main/java/android/pl/myapplication/MainActivity.java
+++ app/src/main/java/android/pl/myapplication/MainActivity.java
@@ -7,10 +7,8 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.net.InetAddress;
@@ -18,27 +16,20 @@ import java.net.Socket;
 
 public class MainActivity extends AppCompatActivity {
 
-    //public static int SERVERPORT;
-    //public static String SERVER_IP;
-
     private ClientThread clientThread;
     private Thread thread;
-    //private LinearLayout msgList;
-    //private Handler handler;
-    //private EditText edMessage;
+
     private EditText SERVER_IP;
     private EditText SERVERPORT;
     private Button connectButton;
     private Button volumeUpButton;
     private Button volumeDownButton;
     private Button muteButton;
-    private Button previousButton;
-    private Button nextButton;
-    private Button playButton;
-    private Button pauseButton;
-    private Button shutdownButton;
-    private Button shutdownCancelButton;
+    private Button shutdownNowButton;
+    private Button shutdownAfterButton;
+    private Button shutdownAfterOffButton;
     private EditText time;
+    private int isMute = 1;
 
 
     @Override
@@ -46,21 +37,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_client);
 
-        //handler = new Handler();
-        //msgList = findViewById(R.id.msgList);
-        //edMessage = findViewById(R.id.edMessage);
 
-        setTitle("Remote Controller - Client");
+        setTitle("Pilot PC - Client");
         connectButton = findViewById(R.id.connectButton);
         volumeUpButton = findViewById(R.id.volumeUPButton);
         volumeDownButton = findViewById(R.id.volumeDownButton);
         muteButton = findViewById(R.id.muteButton);
-        previousButton = findViewById(R.id.previousButton);
-        nextButton = findViewById(R.id.nextButton);
-        playButton = findViewById(R.id.playButton);
-        pauseButton = findViewById(R.id.pauseButton);
-        shutdownButton = findViewById(R.id.shutdownOnButton);
-        shutdownCancelButton = findViewById(R.id.shutdownOffButton);
+        shutdownNowButton = findViewById(R.id.shutdownNowButton);
+        shutdownAfterButton = findViewById(R.id.shutdownAfterButton);
+        shutdownAfterOffButton = findViewById(R.id.shutdownAfterOffButton);
 
         SERVER_IP = findViewById(R.id.editTextIpAddress);
         SERVERPORT = findViewById(R.id.editTextPort);
@@ -73,7 +58,7 @@ public class MainActivity extends AppCompatActivity {
             thread = new Thread(clientThread);
             thread.start();
 
-            Toast.makeText(MainActivity.this, "Connected to Server...", Toast.LENGTH_SHORT).show();
+            Toast.makeText(MainActivity.this, "Connected to Server! :) ", Toast.LENGTH_SHORT).show();
 
             SERVER_IP.setVisibility(View.GONE);
             SERVERPORT.setVisibility(View.GONE);
@@ -82,69 +67,53 @@ public class MainActivity extends AppCompatActivity {
             volumeDownButton.setVisibility(View.VISIBLE);
             muteButton.setVisibility(View.VISIBLE);
             volumeUpButton.setVisibility(View.VISIBLE);
-            playButton.setVisibility(View.VISIBLE);
-            previousButton.setVisibility(View.VISIBLE);
-            pauseButton.setVisibility(View.VISIBLE);
-            nextButton.setVisibility(View.VISIBLE);
+            shutdownNowButton.setVisibility(View.VISIBLE);
             time.setVisibility(View.VISIBLE);
-            shutdownButton.setVisibility(View.VISIBLE);
-            shutdownCancelButton.setVisibility(View.VISIBLE);
+            shutdownAfterButton.setVisibility(View.VISIBLE);
+            shutdownAfterOffButton.setVisibility(View.VISIBLE);
+
+                //Toast.makeText(MainActivity.this, "Problem with connection! :(", Toast.LENGTH_SHORT).show();
+
+
 
         });
 
 
 
-        volumeUpButton.setOnClickListener(view -> {
-            clientThread.sendMessage("up");
-            System.out.println("up");
-        });
+        volumeUpButton.setOnClickListener(view -> clientThread.sendMessage("up"));
 
         volumeDownButton.setOnClickListener(view -> clientThread.sendMessage("down"));
 
-        muteButton.setOnClickListener(view -> clientThread.sendMessage("mute"));
+        muteButton.setOnClickListener(view -> {
 
-        previousButton.setOnClickListener(view -> clientThread.sendMessage("prev"));
+            if(isMute == 1)
+            {
+                clientThread.sendMessage("mute");
+                muteButton.setText("Wył. wyciszenie");
+                isMute = 2;
+            }
+            else
+            {
+                clientThread.sendMessage("mute");
+                muteButton.setText("Wł. wyciszenie");
+                isMute = 1;
+            }
+    });
 
-        nextButton.setOnClickListener(view -> clientThread.sendMessage("next"));
+        shutdownNowButton.setOnClickListener(view -> clientThread.sendMessage("now"));
 
-        playButton.setOnClickListener(view -> clientThread.sendMessage("play"));
+        shutdownAfterButton.setOnClickListener(view -> clientThread.sendMessage(time.getText().toString()));
 
-        pauseButton.setOnClickListener(view -> clientThread.sendMessage("pause"));
-
-        shutdownButton.setOnClickListener(view -> clientThread.sendMessage(time.getText().toString()));
-
-        shutdownCancelButton.setOnClickListener(view -> clientThread.sendMessage("cancel"));
+        shutdownAfterOffButton.setOnClickListener(view -> clientThread.sendMessage("cancel"));
 
 
     }
-
-    /*public TextView textView(String message, int color) {
-        if (null == message || message.trim().isEmpty()) {
-            message = "<Empty Message>";
-        }
-        TextView tv = new TextView(this);
-        tv.setTextColor(color);
-        tv.setText(message + " [" + getTime() + "]");
-        tv.setTextSize(20);
-        tv.setPadding(0, 5, 0, 0);
-        return tv;
-    }*/
-
-    /*public void showMessage(final String message, final int color) {
-        handler.post(new Runnable() {
-            @Override
-            public void run() {
-                msgList.addView(textView(message, color));
-            }
-        });
-    }*/
 
 
 
     class ClientThread implements Runnable {
 
         private Socket socket;
-        private BufferedReader input;
 
         @Override
         public void run() {
@@ -152,22 +121,9 @@ public class MainActivity extends AppCompatActivity {
             try {
                 InetAddress serverAddr = InetAddress.getByName(SERVER_IP.getText().toString());
                 socket = new Socket(serverAddr,Integer.parseInt(SERVERPORT.getText().toString()));
-                //socket = new Socket(SERVER_IP,SERVERPORT);
-
-                while (!Thread.currentThread().isInterrupted()) {
-
-                    this.input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-                    String message = input.readLine();
-                    if (null == message || "Disconnect".contentEquals(message)) {
-                        Thread.interrupted();
-                        message = "Server Disconnected.";
-                        // showMessage(message, Color.RED);
-                        break;
-                    }
-                    //showMessage(message, Color.BLUE);
-                }
 
             } catch (IOException e1) {
+                Thread.interrupted();
                 e1.printStackTrace();
             }
 
